5e3610694df12b557ae8162bfd04c3211d628ef4
Greg MacWilliam <gmacwill77@gmail.com>
2021-01-06 23:11:49 -0500

    use npm package.

--- federation-services/index.js
+++ federation-services/index.js
@@ -4,15 +4,15 @@ const { stitchingDirectivesTransformer } = stitchingDirectives();
 const { buildSchema } = require('graphql');
 const makeServer = require('./lib/make_server');
 const makeRemoteExecutor = require('./lib/make_remote_executor');
-const federationToStitchingSDL = require('./lib/federation_to_stitching');
+const federationToStitchingSDL = require('federation-to-stitching-sdl');
 
 async function makeGatewaySchema() {
   return stitchSchemas({
     subschemaConfigTransforms: [stitchingDirectivesTransformer],
     subschemas: await Promise.all([
-      fetchFederationSubchema(makeRemoteExecutor('http://localhost:4001/graphql', 'products')),
-      fetchFederationSubchema(makeRemoteExecutor('http://localhost:4002/graphql', 'reviews')),
-      fetchFederationSubchema(makeRemoteExecutor('http://localhost:4003/graphql', 'users')),
+      fetchFederationSubchema(makeRemoteExecutor('http://localhost:4001/graphql')),
+      fetchFederationSubchema(makeRemoteExecutor('http://localhost:4002/graphql')),
+      fetchFederationSubchema(makeRemoteExecutor('http://localhost:4003/graphql')),
     ])
   });
 }
--- federation-services/lib/make_remote_executor.js
+++ federation-services/lib/make_remote_executor.js
@@ -4,18 +4,11 @@ const { print } = require('graphql');
 module.exports = function makeRemoteExecutor(url, name) {
   return async ({ document, variables }) => {
     const query = typeof document === 'string' ? document : print(document);
-    const fetchResult = await fetch(url, {
+    const result = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ query, variables }),
     });
-
-    const result = await fetchResult.json();
-    console.log('REQUEST TO:', name);
-    console.log('query', query);
-    console.log('variables', variables);
-    console.log(JSON.stringify(result.data._entities, null, 2));
-    console.log('\n***********\n');
-    return result;
+    return result.json();
   };
 };
