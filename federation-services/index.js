7170c81fa6d855290188e22a03f8c990d9ff8f1a
Greg MacWilliam <gmacwill77@gmail.com>
2021-01-07 10:32:48 -0500

    Federation to Stitching SDL (#17)
    * dunno why this doesnt work...
    
    * demonstrate __typename issue.
    
    * use npm package.
    
    * updated readme.

--- federation-services/index.js
+++ federation-services/index.js
@@ -1,16 +1,29 @@
 const { stitchSchemas } = require('@graphql-tools/stitch');
+const { stitchingDirectives } = require('@graphql-tools/stitching-directives');
+const { stitchingDirectivesTransformer } = stitchingDirectives();
+const { buildSchema } = require('graphql');
 const makeServer = require('./lib/make_server');
 const makeRemoteExecutor = require('./lib/make_remote_executor');
-const makeFederationSubchema = require('./lib/fetch_federation_subschema');
+const federationToStitchingSDL = require('federation-to-stitching-sdl');
 
 async function makeGatewaySchema() {
   return stitchSchemas({
+    subschemaConfigTransforms: [stitchingDirectivesTransformer],
     subschemas: await Promise.all([
-      makeFederationSubchema(makeRemoteExecutor('http://localhost:4001/graphql')),
-      makeFederationSubchema(makeRemoteExecutor('http://localhost:4002/graphql')),
-      makeFederationSubchema(makeRemoteExecutor('http://localhost:4003/graphql')),
+      fetchFederationSubchema(makeRemoteExecutor('http://localhost:4001/graphql')),
+      fetchFederationSubchema(makeRemoteExecutor('http://localhost:4002/graphql')),
+      fetchFederationSubchema(makeRemoteExecutor('http://localhost:4003/graphql')),
     ])
   });
+}
+
+async function fetchFederationSubchema(executor) {
+  const { data } = await executor({ document: '{ _service { sdl } }' });
+  const sdl = federationToStitchingSDL(data._service.sdl);
+  return {
+    schema: buildSchema(sdl),
+    executor,
+  };
 }
 
 makeGatewaySchema().then(schema => makeServer(schema, 'gateway', 4000));
